dccff3534ae5ad9801a9fd776f3791d5c5022873
Kevin Yeung <github@yeung.online>
2017-04-25 22:54:14 +0800

    Compute NRIC check digit

---
+++ nric.clj
@@ -0,0 +1,19 @@
+(def prefix {"S" 0, "T" 4})
+
+(def weights [2 7 6 5 4 3 2])
+
+(def check-digits {0 "J", 1 "Z", 2 "I", 3 "H", 4 "G", 5 "F", 6 "E", 7 "D", 8 "C", 9 "B", 10 "A"})
+
+(defn nric [n]
+  (let [atoms (clojure.string/split n #"")
+        letter-value (prefix (first atoms))
+        numbers (map #(Integer. %) (drop 1 atoms))]
+    (as-> numbers v
+         (map vector weights v)
+         (map (fn [p] (* (first p) (second p))) v)
+         (reduce + v)
+         (+ v letter-value)
+         (rem v 11)
+         (check-digits v)
+         (str n v))))
+
